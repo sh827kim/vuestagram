2e77f702e0d08623d8d11a65a0b01f0337562b40
seonghwa.kim <sh827kim@gmail.com>
2022-06-19 20:39:57 +0900

    image filter add

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -8,11 +8,15 @@
       <span class="profile-name">{{ post.name }} </span>
     </div>
     <div
+      :class="post.filter"
       class="post-body"
       :style="{ backgroundImage: `url(${post.postImage})` }"
+      @click="$store.commit('changeLike')"
     ></div>
     <div class="post-content">
-      <p>{{ post.likes }} Likes</p>
+      <!--      <p>{{ post.likes }} Likes</p>-->
+
+      <p>{{ $store.state.likes }} Likes</p>
       <p>
         <strong>{{ post.name }}</strong> {{ post.content }}
       </p>
