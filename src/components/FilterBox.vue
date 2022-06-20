c3e17e4cb405775349c0da1b4b1c8c5d6097cabb
seonghwa.kim <sh827kim@gmail.com>
2022-06-20 21:05:34 +0900

    vuex added  변경 사항에 대한 커밋 메시지를 입력하십시오. '#' 문자로 시작하는

---
+++ src/components/FilterBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    :class="[filter]"
+    class="filter-item"
+    :style="`background-image:url(${uploadImageURL})`"
+    @click="filterChanged"
+  >
+    <slot name="filter"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "FilterBox",
+  props: {
+    uploadImageURL: String,
+    filter: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    fire() {
+      this.emitter.emit("hi", "hello");
+    },
+    filterChanged() {
+      this.emitter.emit("filter", this.filter);
+    },
+  },
+};
+</script>
+<style>
+.filter-item {
+  width: 100px;
+  height: 100px;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+  background-position: center;
+}
+</style>
